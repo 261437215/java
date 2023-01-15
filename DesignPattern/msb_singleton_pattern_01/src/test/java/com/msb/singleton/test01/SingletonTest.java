f5c3042c1b97603b9870b68d5e27dd0f888db1bd
yinrg <261437215@qq.com>
2023-01-15 17:16:47 +0800

    设计模式-懒汉模式代码提交

---
+++ DesignPattern/msb_singleton_pattern_01/src/test/java/com/msb/singleton/test01/SingletonTest.java
@@ -0,0 +1,25 @@
+package com.msb.singleton.test01;
+
+import com.msb.singleton.demo1.Singleton_01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author Created by yinrg on 2023-01-15 16:27
+ * @fileSimpleName SingletonTest
+ * @fileName com.msb.singleton.test01.SingletonTest
+ * @description
+ */
+
+@Slf4j
+public class SingletonTest {
+
+    // 饿汉式测试
+    @Test
+    public void test01(){
+        Singleton_01 instance = Singleton_01.getInstance();
+        Singleton_01 instance2 = Singleton_01.getInstance();
+        log.info("比较对象内存地址是否相等：{}",instance == instance2);
+    }
+}
