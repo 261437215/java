7389523cab2e2e9f853805943f2853ad8c778ad3
yinrg <261437215@qq.com>
2023-01-20 10:59:13 +0800

    设计模式-工厂模式代码提交

--- DesignPattern/msb_factory_pattern_02/src/main/java/com/msb/factory/schemes02/factory/FreeGoodsFactory.java
+++ DesignPattern/msb_factory_pattern_02/src/main/java/com/msb/factory/schemes02/factory/FreeGoodsFactory.java
@@ -10,6 +10,12 @@ import com.msb.factory.schemes02.service.impl.YouKuMemberFreeGoods;
  * @fileSimpleName FreeGoodsFactory
  * @fileName com.msb.factory.schemes02.factory.FreeGoodsFactory
  * @description 具体工厂:生成免费商品
+ * 简单工厂模式的优点：
+ *  封装了创建对象的过程,可以通过参数直接获取对象,把对象的创建和业务逻辑分开,避免了可能会直接修改客户代码的问题。
+ *  如果要实现新产品,直接修改工厂类,不需要再源代码中进行修改,降低了客户端修改代码的可能性,更加容易扩展。
+ *
+ * 简单工厂的缺点：
+ *  在增加新产品的时候还是要修改工厂类的代码,违背了开闭原则。
  */
 public class FreeGoodsFactory {
     public static IFreeGoods getInstance(Integer awardType) {
