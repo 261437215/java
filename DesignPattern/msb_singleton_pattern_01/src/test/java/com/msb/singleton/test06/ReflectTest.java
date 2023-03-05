1f947521f20c45d7f10314df2289abe68c398511
yinrg <261437215@qq.com>
2023-03-05 19:07:10 +0800

    设计模式-单例模式代码添加详细注释

--- DesignPattern/msb_singleton_pattern_01/src/test/java/com/msb/singleton/test06/ReflectTest.java
+++ DesignPattern/msb_singleton_pattern_01/src/test/java/com/msb/singleton/test06/ReflectTest.java
@@ -2,6 +2,7 @@ package com.msb.singleton.test06;
 
 import com.msb.singleton.demo6.Singleton_06;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
 import java.lang.reflect.Constructor;
 
@@ -11,16 +12,19 @@ import java.lang.reflect.Constructor;
  * @fileSimpleName TestReflect
  * @fileName com.msb.singleton.test05.TestReflect
  * @description 反射对单例的破坏
+ * 枚举对象中不能使用newInstance()的方式去创建枚举对象,会在底层代码中抛出异常
  */
 @Slf4j
 public class ReflectTest {
     public static void main(String[] args) throws Exception {
         Class<Singleton_06> singleton06Class = Singleton_06.class;
+        // 获取构造方法的对象
         Constructor<Singleton_06> constructor = singleton06Class.getDeclaredConstructor();
         // 设置为true以后就可以对类中的私有成员进行操作了
         constructor.setAccessible(true);
         // 反射不能创建枚举对象
-        Singleton_06 singleton06 = constructor.newInstance(String.class,int.class);
-        log.info(singleton06.toString());
+//        Singleton_06 instance = constructor.newInstance();
+        Singleton_06 instance = constructor.newInstance(String.class, int.class);
+        log.info(instance.toString());
     }
 }
